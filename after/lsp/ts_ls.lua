4d0f06454ba2bafaf66b7c186ef9c2bdc2792f58
mafiasheep <jzenk1992@gmail.com>
2026-03-15 21:58:34 -0500

    changing locations of lsp returns

---
+++ after/lsp/ts_ls.lua
@@ -0,0 +1,41 @@
+return {
+    root_dir = function(bufnr, on_dir)
+        -- The project root is where the LSP can be started from
+        -- As stated in the documentation above, this LSP supports monorepos and simple projects.
+        -- We select then from the project root, which is identified by the presence of a package
+        -- manager lock file.
+        local root_markers = { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb', 'bun.lock' }
+
+        -- NOTE: THIS IMPLEMENTATION DIFFERS FROM THE DEFAULT IMPLEMENTATION IN NVIM-LSPCONFIG
+        -- IDK WHY THEY BROKE IT WITH { root_markers, { '.git' } }
+        root_markers = vim.list_extend(root_markers, { '.git' })
+        -- We fallback to the current working directory if no project root is found
+        local project_root = vim.fs.root(bufnr, root_markers) or vim.fn.getcwd()
+
+        on_dir(project_root)
+    end,
+    settings = {
+        typescript = {
+            inlayHints = {
+                includeInlayParameterNameHints = 'all',
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+            }
+        },
+        javascript = {
+            inlayHints = {
+                includeInlayParameterNameHints = 'all',
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+            }
+        },
+    },
+}
