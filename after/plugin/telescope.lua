5c7e957d442852d3b0c18962f3eb235eed139e8d
mafiasheep <jzenk1992@gmail.com>
2026-03-16 14:09:08 -0500

    adding file paths to ignore with telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,5 +2,18 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope git files' })
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+    builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+
+require('telescope').setup({
+    defaults = {
+        file_ignore_patterns = {
+            "node_modules",
+            "dist",
+            "build",
+            ".git",
+            "venv",
+            "__pycache__",
+        },
+    },
+})
